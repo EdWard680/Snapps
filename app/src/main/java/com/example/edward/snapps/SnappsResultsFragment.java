54ddb8a3d6a2046784cd2b9fd9994041c6c52a49
crocman <siriusjjust@gmail.com>
2015-07-14 09:41:29 -0400

    Camera works and displays picture

--- app/src/main/java/com/example/edward/snapps/SnappsResultsFragment.java
+++ app/src/main/java/com/example/edward/snapps/SnappsResultsFragment.java
@@ -9,13 +9,12 @@ import android.widget.ImageView;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-
+import android.util.Log;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class SnappsResultsFragment extends Fragment {
-
     private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
     private static ImageView iv;
 
@@ -25,9 +24,12 @@ public class SnappsResultsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        Log.wtf("OnCreateView", "View Created");
         View rootView =  inflater.inflate(R.layout.fragment_snapps_results, container, false);
         iv = (ImageView) rootView.findViewById(R.id.cameraResult);
         Bitmap picture = BitmapFactory.decodeFile(MainActivity.getImageFile().toString());
+        iv.setImageBitmap(picture);
         return rootView;
     }
+
 }
